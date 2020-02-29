c8937a045b3bc00c125a9bd281b75cf175e2d3fe
Monika <mroznere@gmail.com>
2020-02-29 10:38:20 -0500

    Launch file can now call the camera_info_publisher script.

--- scripts/publish_camera_info.py
+++ scripts/publish_camera_info.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import rospkg
 import yaml
 from sensor_msgs.msg import CameraInfo
 
@@ -9,9 +10,12 @@ if __name__ == '__main__':
         rospy.init_node('camera_info_publisher', anonymous=True)
         pub = rospy.Publisher('camera/camera_info', CameraInfo, queue_size=1)
         rate = rospy.Rate(10)
+        rospack = rospkg.RosPack()
+
+        camera_params_file = rospack.get_path('stag_ros') + "/param/camera_params.yaml"
 
         # Load data from file
-        with open("param/camera_params.yaml", "r") as file_handle:
+        with open(camera_params_file, "r") as file_handle:
             calib_data = yaml.load(file_handle)
         # Parse
         camera_info_msg = CameraInfo()

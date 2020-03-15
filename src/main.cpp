1867f0a9f2d132a50138fe5f9f66402c342e062c
slensgra <sam.lensgraf@gmail.com>
2020-03-15 06:13:43 -0400

    add main back in

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "opencv2/opencv.hpp"
+#include "Stag.h"
+
+
+int main(int argc, char** argv)
+{
+	cv::Mat image = cv::imread("1.png", CV_LOAD_IMAGE_GRAYSCALE);
+
+	Stag stag(15, 7, true);
+
+	stag.detectMarkers(image);
+	stag.logResults("log/");
+}
